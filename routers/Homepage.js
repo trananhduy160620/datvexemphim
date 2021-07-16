e523b1cd0109130953780b8485e6bcea847cb834
CoBocXo <hiepdo1082@gmail.com>
2021-07-17 00:22:08 +0700

    fix ticket page

--- routers/Homepage.js
+++ routers/Homepage.js
@@ -5,6 +5,9 @@ const Home = require("../controllers/Home");
 const router = express.Router();
 
 router.get("/", Home.getHomePage);
+router.get('/redex',function(req,res){
+    res.redirect('.');
+});
 router.get("/register", Home.getRegister);
 
 module.exports = router;
